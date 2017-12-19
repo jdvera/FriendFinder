d1d9fb3301103f12db7caf21bfa54de7ffd0b0ee
jdvera <jdvera2015@gmail.com>
2017-12-19 12:45:24 -0600

    added reset button

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,10 +4,13 @@ module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
 		res.json(friendArr);
 	});
+	app.get("/api/reset", function(req, res) {
+		friendArr = require("../data/friends.js");
+		res.json(friendArr);
+	});
 	app.post("/api/friends", function(req, res) {
 		var newFriend = req.body;
 		friendArr.push(newFriend);
 		res.json("You did a thing");
 	});
 };
-
